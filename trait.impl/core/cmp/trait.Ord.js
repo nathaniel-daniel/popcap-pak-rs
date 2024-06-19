3af679a33dc19cb17aafa664abea77afba1e5eb6
nathaniel-daniel <nathaniel-daniel@users.noreply.github.com>
2024-06-19 03:05:03 +0000

    deploy: 4cddc43e41d716889d5d8da2807fcdfa341c1e2b

---
+++ trait.impl/core/cmp/trait.Ord.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"popcap_pak":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.78.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"popcap_pak/file_time/struct.FileTime.html\" title=\"struct popcap_pak::file_time::FileTime\">FileTime</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
