ae6cc1606e6fbbd9d555fd278a3c53442068fa50
nathaniel-daniel <nathaniel-daniel@users.noreply.github.com>
2024-09-21 22:58:07 +0000

    deploy: 86fabd5813076982ca00c0416ddf511282afeb9a

---
+++ search.desc/popcap_pak_wasm/popcap_pak_wasm-desc-0-.js
@@ -0,0 +1,1 @@
+searchState.loadedDescShard("popcap_pak_wasm", 0, "Returns the argument unchanged.\nReturns the argument unchanged.\nCalls <code>U::from(self)</code>.\nCalls <code>U::from(self)</code>.\nMake a new Pak.")
---
+++ trait.impl/core/hash/trait.Hash.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"popcap_pak":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.81.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"popcap_pak/file_time/struct.FileTime.html\" title=\"struct popcap_pak::file_time::FileTime\">FileTime</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
