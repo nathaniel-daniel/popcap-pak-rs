743911e2a1e570109b53aca989dac1230e42a201
Nathaniel Daniel <nathaniel.daniel12@gmail.com>
2025-12-27 15:36:27 -0800

    Fmt demo

--- popcap-pak-wasm/demo/src/App.svelte
+++ popcap-pak-wasm/demo/src/App.svelte
@@ -1,14 +1,15 @@
 <script>
-    import LoadedApp from './LoadedApp.svelte';
-    
-    let PopCapPak = import('../pkg');
+  import LoadedApp from "./LoadedApp.svelte";
+
+  let PopCapPak = import("../pkg");
 </script>
+
 <div style="margin: 0.2em; display: flex; flex: 1;">
-    {#await PopCapPak}
-        Loading...
-    {:then PopCapPak}
-        <LoadedApp {PopCapPak} />
-    {:catch error}
-        {error.message}
-    {/await}
+  {#await PopCapPak}
+    Loading...
+  {:then PopCapPak}
+    <LoadedApp {PopCapPak} />
+  {:catch error}
+    {error.message}
+  {/await}
 </div>
--- popcap-pak-wasm/demo/src/DirectoryNode.svelte
+++ popcap-pak-wasm/demo/src/DirectoryNode.svelte
@@ -1,44 +1,52 @@
 <script>
-    import { createEventDispatcher } from 'svelte';
-    
-    export let name;
-    export let node;
-    export let PopCapPak;
-    
-    let expanded = false;
-    let isLeaf = node instanceof PopCapPak.Entry;
-    const dispatch = createEventDispatcher();
-    
-    function handleOnClick(event) {
-        if(isLeaf) {
-            dispatch('view', {
-                node,
-            });
-        }
+  import { createEventDispatcher } from "svelte";
+
+  export let name;
+  export let node;
+  export let PopCapPak;
+
+  let expanded = false;
+  let isLeaf = node instanceof PopCapPak.Entry;
+  const dispatch = createEventDispatcher();
+
+  function handleOnClick(event) {
+    if (isLeaf) {
+      dispatch("view", {
+        node,
+      });
     }
-    
-    function toggleExpanded() {
-        if(!isLeaf) 
-            expanded = !expanded;
-    }
+  }
+
+  function toggleExpanded() {
+    if (!isLeaf) expanded = !expanded;
+  }
 </script>
+
 <div>
-    <div>
-        {#if !isLeaf}
-            <div style="display: inline; cursor: pointer;" on:click={toggleExpanded} on:keypress={toggleExpanded}>
-                {(expanded ? '-' : '+')}
-            </div>
-        {/if}
-        
-        <div on:click={handleOnClick} on:keypress={handleOnClick} style="display: inline;">
-            {name}
-        </div>
-    </div>
-    {#if expanded}
-        <div style="padding-left: 1em;">
-            {#each Object.entries(node) as [name, node]}
-                <svelte:self {name} {node} {PopCapPak} on:view />
-            {/each}
-        </div>
+  <div>
+    {#if !isLeaf}
+      <div
+        style="display: inline; cursor: pointer;"
+        on:click={toggleExpanded}
+        on:keypress={toggleExpanded}
+      >
+        {expanded ? "-" : "+"}
+      </div>
     {/if}
+
+    <div
+      on:click={handleOnClick}
+      on:keypress={handleOnClick}
+      style="display: inline;"
+    >
+      {name}
+    </div>
+  </div>
+  {#if expanded}
+    <div style="padding-left: 1em;">
+      {#each Object.entries(node) as [name, node]}
+        <svelte:self {name} {node} {PopCapPak} on:view />
+      {/each}
+    </div>
+  {/if}
 </div>
