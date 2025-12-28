0b5a84c4beffb961fdf5fdd1b344f3908f1195e0
Nathaniel Daniel <nathaniel.daniel12@gmail.com>
2025-12-27 16:58:51 -0800

    Add custom plugin

--- popcap-pak-wasm/demo/vite.config.js
+++ popcap-pak-wasm/demo/vite.config.js
@@ -1,31 +1,32 @@
 import { defineConfig } from "vite";
 import wasm from "vite-plugin-wasm";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import { run } from "vite-plugin-run";
 import path from "node:path";
+import { exec } from "node:child_process";
+import { promisify } from "node:util";
+
+const wasmPackCommandArgs = [
+  "build",
+  "..",
+  "--release",
+  // These are enabled by default in the Rust std anyway.
+  "--reference-types",
+  "--out-dir",
+  path.join(__dirname, "pkg"),
+];
+
+function buildWasmPack() {
+  return {
+    name: "build-wasm-pack",
+
+    async buildStart(options) {
+      await promisify(exec)("wasm-pack " + wasmPackCommandArgs.join(" "), {
+        stdio: "ignore",
+      });
+    },
+  };
+}
 
 export default defineConfig({
-  plugins: [
-    run({
-      input: [
-        {
-          name: "Wasm-Pack Build",
-          run: [
-            "wasm-pack",
-            "build",
-            "..",
-            "--release",
-            // These are enabled by default in the Rust std anyway.
-            "--reference-types",
-            "--out-dir",
-            path.join(__dirname, "pkg"),
-          ],
-          pattern: ["../**/*.rs", "../**/*.toml", "../**/*.lock"],
-        },
-      ],
-      // silent: false,
-    }),
-    svelte({}),
-    wasm(),
-  ],
+  plugins: [buildWasmPack(), svelte({}), wasm()],
 });
