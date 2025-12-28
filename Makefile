bf821b305334a5a3e3bdb65fefc50ac9daaeef87
nathaniel-daniel <80441888+nathaniel-daniel@users.noreply.github.com>
2025-12-27 17:03:40 -0800

    Fix demo (#57)
    * Switch to vite
    
    * Change how wasm-pack is installed
    
    * Add wasm-pack to build process
    
    * Add custom plugin

--- Makefile
+++ Makefile
@@ -2,11 +2,14 @@
 
 export NODE_ENV =
 
+.PHONY: build-wasm
 build-wasm:
 	wasm-pack build --release popcap-pak-wasm --out-dir pkg
-	
+
+.PHONY: setup-wasm-demo	
 setup-wasm-demo:
 	cd popcap-pak-wasm/demo && npm install --save-dev
 
+.PHONY: build-wasm-demo
 build-wasm-demo:
 	cd popcap-pak-wasm/demo && npm run build
---
+++ popcap-pak-wasm/demo/vite.config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from "vite";
+import wasm from "vite-plugin-wasm";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import path from "node:path";
+import { exec } from "node:child_process";
+import { promisify } from "node:util";
+
+const wasmPackCommandArgs = [
+  "build",
+  "..",
+  "--release",
+  // These are enabled by default in the Rust std anyway.
+  "--reference-types",
+  "--out-dir",
+  path.join(__dirname, "pkg"),
+];
+
+function buildWasmPack() {
+  return {
+    name: "build-wasm-pack",
+
+    async buildStart(options) {
+      await promisify(exec)("wasm-pack " + wasmPackCommandArgs.join(" "), {
+        stdio: "ignore",
+      });
+    },
+  };
+}
+
+export default defineConfig({
+  plugins: [buildWasmPack(), svelte({}), wasm()],
+});
