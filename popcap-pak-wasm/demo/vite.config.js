e6886b8d98fa72271a8235cae0b680144172f78b
Nathaniel Daniel <nathaniel.daniel12@gmail.com>
2025-12-27 16:37:45 -0800

    Add wasm-pack to build process

--- popcap-pak-wasm/demo/vite.config.js
+++ popcap-pak-wasm/demo/vite.config.js
@@ -1,7 +1,31 @@
 import { defineConfig } from "vite";
 import wasm from "vite-plugin-wasm";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { run } from "vite-plugin-run";
+import path from "node:path";
 
 export default defineConfig({
-  plugins: [svelte({}), wasm()],
+  plugins: [
+    run({
+      input: [
+        {
+          name: "Wasm-Pack Build",
+          run: [
+            "wasm-pack",
+            "build",
+            "..",
+            "--release",
+            // These are enabled by default in the Rust std anyway.
+            "--reference-types",
+            "--out-dir",
+            path.join(__dirname, "pkg"),
+          ],
+          pattern: ["../**/*.rs", "../**/*.toml", "../**/*.lock"],
+        },
+      ],
+      // silent: false,
+    }),
+    svelte({}),
+    wasm(),
+  ],
 });
